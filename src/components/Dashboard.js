50b811889e2bc68f9e3d256a969bdb9f41b7c3ef
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-14 02:01:22 -0500

    Import Loading into Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
 
 import classnames from "classnames";
 
 class Dashboard extends Component {
   state = { loading: true };
-  
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
