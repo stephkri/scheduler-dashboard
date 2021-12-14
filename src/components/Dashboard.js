732ad2d0e5f06c14d4093387ea6075f9ccc0629d
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-14 02:04:25 -0500

    Add fake data to Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,29 @@ import Loading from "./Loading";
 
 import classnames from "classnames";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 class Dashboard extends Component {
   state = { loading: true };
 
@@ -12,7 +35,7 @@ class Dashboard extends Component {
     if (this.state.loading) {
       return <Loading />;
     }
-    
+
     return <main className={dashboardClasses} />;
   }
 }
