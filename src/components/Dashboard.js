39ee8065c50839f967f93c756dee4e81a3cf8a0c
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-14 02:39:34 -0500

    Fix binding

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -50,10 +50,9 @@ class Dashboard extends Component {
       .map(panel => (
       <Panel
         key={panel.id}
-        id={panel.id}
         label={panel.label}
         value={panel.value}
-        onSelect={this.selectPanel}
+        onSelect={event => this.selectPanel(panel.id)}
       />
     ));
 
