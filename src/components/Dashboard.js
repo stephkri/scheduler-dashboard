83aa02dd81e3d16e824cf006c439d7f9b81ca331
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-14 20:35:47 -0500

    Add onmessage handler

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -65,6 +65,16 @@ class Dashboard extends Component {
     });
 
     this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
   };
 
   componentDidUpdate(previousProps, previousState) {
