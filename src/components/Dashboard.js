5724071628e762834e0cd42c96e01139e907a5c5
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-14 20:25:23 -0500

    Put getValue keys in data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -15,22 +15,22 @@ const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
